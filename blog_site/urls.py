7302d0fd6d2be98417958dee058a65a8bb5c97c0
getachewzemene <getchze1221@gmail.com>
2023-01-21 10:44:06 +0300

    replace static images with stored file on db and update the path

--- blog_site/urls.py
+++ blog_site/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls")),
-]
+] + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 """ 
     http://localhost:8000/
     http://localhost:8000/posts
