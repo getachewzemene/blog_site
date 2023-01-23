170559c71608408854b3015b76be223465fb0ccb
getachewzemene <getchze1221@gmail.com>
2023-01-23 12:03:22 +0300

    register comment in admin site and display and add style to comments

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Author, Post, Tag
+from .models import Author, Post, Tag, Comment
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class PostAdmin(admin.ModelAdmin):
 admin.site.register(Author)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment)
